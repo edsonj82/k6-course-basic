80826ab2fe096dcc7112c9633b093e7cceac0b2a
Edson José dos Santos <edsonj82@outlook.com>
2024-02-29 17:34:10 -0300

    defintions tresholds & fix bug timeout (2000ms)

--- tests/signup.js
+++ tests/signup.js
@@ -5,7 +5,11 @@ import uuid from './libs/uuid.js'
 
 export const options = {
   vus: 10,
-  duration: '30s'
+  duration: '30s',
+  thresholds: {
+    http_req_duration: ['p(95)<2000'], //95% das requisições devem responder em até 2 milisegundos
+    http_req_failed: ['rate<0.01 '] //1% das requisições podem ocorrer erro
+  }
 }
 
 // export const options = {
